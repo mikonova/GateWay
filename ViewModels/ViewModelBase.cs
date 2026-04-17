7bfa362f3e8e938eb46698bbb602dc4fc50759fe
mikonova <mikk4business@gmail.com>
2026-04-17 12:22:58 +0300

    initial structure

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GateWay.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GateWay.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
