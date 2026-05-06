0bfd3cd43b34bc470edd12322bf19c38f06e2b03
mikonova <mikk4business@gmail.com>
2026-05-07 00:37:12 +0300

    Created UI skeleton

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,47 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace GateWay.Views;
 
 public partial class MainWindow : Window
 {
+    public bool IsFullScreen = false;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void PaneExpand_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (SidePane.IsPaneOpen)
+        {
+            SidePane.IsPaneOpen = false;
+        }
+        else SidePane.IsPaneOpen = true;
+    }
+
+    private void BtnClose_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void BtnFullscreen_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if(this.WindowState == WindowState.FullScreen)
+        {
+            this.WindowState = WindowState.Normal;
+            BtnFullscreen.Content = "□";
+        }
+        else
+        {
+            this.WindowState = WindowState.FullScreen;
+            BtnFullscreen.Content = "■";
+        }
+    }
+
+    private void BtnToTray_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
     }
 }
