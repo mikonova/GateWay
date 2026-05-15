295f79e8db580f829ddcca937325bb8d9bdf8df7
mikonova <mikk4business@gmail.com>
2026-05-15 21:38:15 +0300

    minor variable rights changes

--- ColorPalette.cs
+++ ColorPalette.cs
@@ -1,18 +1,18 @@
 using Avalonia.Media;
 using Avalonia.Themes.Fluent;
 
-namespace Colors;
+namespace ColorPalette;
 // aqua nebula palette
-public class ColorPaletteNebula
+public static class ColorPaletteNebula
 {
     // midnight abyss
-    private readonly SolidColorBrush _backgroundColor = new SolidColorBrush(Color.Parse("#061826"));
+    public static readonly SolidColorBrush BackgroundColor = new SolidColorBrush(Color.Parse("#061826"));
     // atlantic steel
-    private readonly SolidColorBrush _bnBgColor = new SolidColorBrush(Color.Parse("#IC4E75"));
+    public static readonly SolidColorBrush OnBgColor = new SolidColorBrush(Color.Parse("#IC4E75"));
     // smoky white
-    private readonly SolidColorBrush _highlightColor = new SolidColorBrush(Color.Parse("#40f5f5f5"));
+    public static readonly SolidColorBrush HighlightColor = new SolidColorBrush(Color.Parse("#40f5f5f5"));
     //tidepool teal
-    private readonly SolidColorBrush _chatCloudColor = new SolidColorBrush(Color.Parse("#2FA0C6"));
+    public static readonly SolidColorBrush ChatCloudColor = new SolidColorBrush(Color.Parse("#2FA0C6"));
     // glacier mist
-    private readonly SolidColorBrush _glacierMistColor = new SolidColorBrush(Color.Parse("#2FA0C6"));
+    public static readonly SolidColorBrush GlacierMistColor = new SolidColorBrush(Color.Parse("#2FA0C6"));
 }
