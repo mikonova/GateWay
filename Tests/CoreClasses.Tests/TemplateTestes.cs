3a83fa90bfe974957e2d0aa1f8be98da8f7d70f6
unknown <NteliFake>
2026-05-21 21:51:35 +0300

    Bueeeee

---
+++ Tests/CoreClasses.Tests/TemplateTestes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CoreClasses.Tests
+{
+    public class TemplateTestes
+    {
+        private readonly Templates tmpl;
+        public TemplateTestes()
+        {
+            tmpl = new Templates("C:\\Users\\nteli\\Desktop\\GateWayTests");
+        }
+
+        [Fact]
+        public void TryToLoad()
+        {
+            try
+            {
+                tmpl.LoadAllChats();
+                Assert.True(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Упало с ошибкой: {ex.Message}");
+            }
+
+
+        }
+        //[Fact]
+
+
+       }
+}
